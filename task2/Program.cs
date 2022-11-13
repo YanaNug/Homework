2d015c355ac244c1840b08e588f16ec2307f509d
Yana <yana229910nug@yandex.ru>
2022-11-13 22:42:23 +0300

    task №2

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 2 числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if(a >= b)
+{
+    Console.WriteLine($"max={a} , min={b}");
+}
+else
+{
+    Console.WriteLine($"max={b} , min={a}");
+}
