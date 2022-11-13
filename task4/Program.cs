10357db021d78bd621a29b8e9ca2366a0e1d0461
Yana <yana229910nug@yandex.ru>
2022-11-13 22:42:47 +0300

    task №4

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите 3 числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
