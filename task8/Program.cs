2ed0f53ef5719f32691832857eb3caaabb6d75f8
Yana <yana229910nug@yandex.ru>
2022-11-14 23:09:29 +0300

    add task8

---
+++ task8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Console.WriteLine("Введите число:");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// while (i <= N)
+// {
+//     if (i % 2 == 0)
+//     {
+//         Console.Write(i+ " ");
+//     }
+//     i++;
+// }
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= N)
+{
+    Console.Write(i+ " ");
+       i+=2;
+}
